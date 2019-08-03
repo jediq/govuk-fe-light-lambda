19bebda5c146a2ba16c3ed80f3d12a6dd32c8dff
Ricky Walker <ricky.walker@jediq.com>
2019-08-03 08:57:41 +0100

    trying to trigger lambda

--- copyStaticAssets.ts
+++ copyStaticAssets.ts
@@ -1,6 +1,5 @@
 import * as shell from "shelljs";
 
-shell.cp("-R", "src/public/fonts", "dist/public/");
 shell.cp("-R", "src/public/images", "dist/public/");
 shell.cp("-R", "src/templates", "dist/templates/");
 shell.cp("-R", "node_modules/govuk-frontend/assets", "dist/");

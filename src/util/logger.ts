532a3b373ebef4434ffc679f550784bfff3885a1
Ricky Walker <ricky.walker@jediq.com>
2019-08-03 10:47:56 +0100

    removed file write for logging

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,19 +1,17 @@
 import { Logger, LoggerOptions, transports } from "winston";
 
 const options: LoggerOptions = {
-    transports: [
-        new transports.Console({
-            level: process.env.NODE_ENV === "production" ? "error" : "debug"
-        }),
-        new transports.File({ filename: "debug.log", level: "debug" })
-    ]
+  transports: [
+    new transports.Console({
+      level: process.env.NODE_ENV === "production" ? "error" : "debug"
+    })
+  ]
 };
 
 const logger = new Logger(options);
 
 if (process.env.NODE_ENV !== "production") {
-    logger.debug("Logging initialized at debug level");
+  logger.debug("Logging initialized at debug level");
 }
 
 export default logger;
-    

39277d00967edc71dc54a87dad479abef9126cf6
Ricky Walker <ricky.walker@jediq.com>
2019-08-03 18:15:43 +0100

    using lambda environment variables

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -5,9 +5,9 @@ import winston from "winston";
 const { createLogger, format, transports } = winston;
 const { combine, timestamp, label, prettyPrint } = format;
 var logger = createLogger({
-    level: process.env.npm_config_debug == "true" ? "debug" : "info",
-    format: format.combine(format.colorize(), format.simple()),
-    transports: [new transports.Console()]
+  level: process.env.npm_config_debug == "true" || process.env.debug == "true" ? "debug" : "info",
+  format: format.combine(format.colorize(), format.simple()),
+  transports: [new transports.Console()]
 });
 
 export default logger;

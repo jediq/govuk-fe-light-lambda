2574ed6bdd3fbfe320a7125e6e47e9951b8cf509
Ricky Walker <ricky.walker@jediq.com>
2019-08-03 17:54:55 +0100

    rebuilt with new deps

--- copyStaticAssets.ts
+++ copyStaticAssets.ts
@@ -2,5 +2,5 @@ import * as shell from "shelljs";
 
 shell.cp("-R", "src/public/images", "dist/public/");
 shell.cp("-R", "src/templates", "dist/templates/");
-shell.cp("-R", "node_modules/govuk-frontend/assets", "dist/");
-shell.cp("-R", "node_modules/govuk-frontend/all.js", "dist/assets/");
+shell.cp("-R", "node_modules/govuk-frontend/govuk/assets", "dist/");
+shell.cp("-R", "node_modules/govuk-frontend/govuk/all.js", "dist/assets/");
--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,17 +1,31 @@
-import { Logger, LoggerOptions, transports } from "winston";
+//var winston = require("winston");
 
-const options: LoggerOptions = {
-  transports: [
-    new transports.Console({
-      level: process.env.NODE_ENV === "production" ? "error" : "debug"
-    })
-  ]
-};
+import winston from "winston";
 
-const logger = new Logger(options);
-
-if (process.env.NODE_ENV !== "production") {
-  logger.debug("Logging initialized at debug level");
-}
+const { createLogger, format, transports } = winston;
+const { combine, timestamp, label, prettyPrint } = format;
+var logger = createLogger({
+    level: process.env.npm_config_debug == "true" ? "debug" : "info",
+    format: format.combine(format.colorize(), format.simple()),
+    transports: [new transports.Console()]
+});
 
 export default logger;
+
+// import { Logger, LoggerOptions, transports } from "winston";
+
+// const options: LoggerOptions = {
+//   transports: [
+//     new transports.Console({
+//       level: process.env.NODE_ENV === "production" ? "error" : "debug"
+//     })
+//   ]
+// };
+
+// const logger = new Logger(options);
+
+// if (process.env.NODE_ENV !== "production") {
+//   logger.debug("Logging initialized at debug level");
+// }
+
+// export default logger;

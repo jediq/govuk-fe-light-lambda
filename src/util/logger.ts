de05cf7efdbdce28c5ebded2ccb19c37defdada5
Ricky Walker <ricky.walker@jediq.com>
2019-08-04 10:44:10 +0100

    changed CSS path locator

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -4,6 +4,7 @@ import winston from "winston";
 
 const { createLogger, format, transports } = winston;
 const { combine, timestamp, label, prettyPrint } = format;
+console.log(`environment: process.env.npm_config_debug=${process.env.npm_config_debug} : process.env.debug=${process.env.debug}`);
 var logger = createLogger({
   level: process.env.npm_config_debug == "true" || process.env.debug == "true" ? "debug" : "info",
   format: format.combine(format.colorize(), format.simple()),

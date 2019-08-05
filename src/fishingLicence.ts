2140e611b7bbe3531c7f491e1562833cb12e66e5
Ricky Walker <ricky.walker@jediq.com>
2019-08-05 15:31:35 +0100

    Adding fishingLicence

---
+++ src/fishingLicence.ts
@@ -0,0 +1,26 @@
+module.exports = {
+  name: "Get a fishing licence",
+  targetUrl: "https://someurl.gov.uk",
+  gdsPhase: "beta",
+  firstPage: "type",
+  successMessage: "Your details where saved correctly",
+  failureMessage: "Your details failed to be saved",
+  cookieSecret: "8y/B?D(G+KbPeShVmYq3t6w9z$C&F)H@",
+  pages: [
+    {
+      id: "type",
+      description: "Which fishing licence do you want to get?",
+      nextPage: () => "channel-selection",
+      preRequisiteData: [],
+      items: [
+        {
+          id: "typeField",
+          type: "radio",
+          options: ["1 day", "8 days", "12 months"],
+          validator: ".+",
+          error: "Choose the duration"
+        }
+      ]
+    }
+  ]
+};

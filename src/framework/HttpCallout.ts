84be46d03a9ecec07de76ae45d4d71343305bcc8
Ricky Walker <ricky.walker@jediq.com>
2019-08-26 14:13:53 +0100

    added uuid for session ID

--- src/framework/HttpCallout.ts
+++ src/framework/HttpCallout.ts
@@ -4,35 +4,36 @@ import logger from "./util/logger";
 import environment from "./util/environment";
 import * as handlebars from "handlebars";
 import got from "got";
+import _ from "lodash";
 
 export class HttpCallout {
-  public async call(call: HttpCall, context: Context) {
-    var urlTemplate = handlebars.compile(!environment.debug ? call.url : call.debugUrl);
-    var url = urlTemplate({ context });
-    logger.info("calling preValidation url : " + url);
-    var response = "";
-    if (call.method == "POST") {
-      var body = _.get(context, call.bodySelector);
-      response = await this.makePostCall(url, body);
-    } else {
-      response = await this.makeGetCall(url);
+    public async call(call: HttpCall, context: Context) {
+        var urlTemplate = handlebars.compile(!environment.debug ? call.url : call.debugUrl);
+        var url = urlTemplate({ context });
+        logger.info("calling preValidation url : " + url);
+        var response = "";
+        if (call.method == "POST") {
+            var body = _.get(context, call.bodySelector);
+            response = await this.makePostCall(url, body);
+        } else {
+            response = await this.makeGetCall(url);
+        }
+
+        logger.debug("response.body : " + response);
+        response = JSON.parse(response);
+
+        if (call.postProcess) {
+            response = call.postProcess(response);
+        }
+
+        return response;
     }
 
-    logger.debug("response.body : " + response);
-    response = JSON.parse(response);
-
-    if (call.postProcess) {
-      response = call.postProcess(response);
+    private async makeGetCall(url: string) {
+        return (await got(url)).body;
     }
 
-    return response;
-  }
-
-  private async makeGetCall(url: string) {
-    return (await got(url)).body;
-  }
-
-  private async makePostCall(url: string, body: any) {
-    return (await got(url)).body;
-  }
+    private async makePostCall(url: string, body: any) {
+        return (await got(url)).body;
+    }
 }
--- test/pages/Page.test.ts
+++ test/pages/Page.test.ts
@@ -9,29 +9,29 @@ import nunjucks from "nunjucks";
 import fs from "fs";
 
 (function() {
-  test("test base page rendering", () => {
-    let context = new Context(null);
-    let page = new Page();
-    var output = page.render(context);
-    expect(output).toContain("document.body.className = document.body.className");
-  });
-
-  test("test gov uk page rendering", () => {
-    nunjucks.configure(["node_modules/govuk-frontend/", "src/framework/"], {
-      autoescape: true
-    });
-    let context = new Context(null);
-    let page = new GovUkPage();
-    var output = page.render(context);
-    expect(output).toContain("govuk-template");
-  });
-
-  test("test nunjucks loading", () => {
-    nunjucks.configure(["node_modules/govuk-frontend/"], {
-      autoescape: true
+    test("test base page rendering", () => {
+        let context = new Context(null);
+        let page = new Page();
+        var output = page.render(context);
+        expect(output).toContain("document.body.className = document.body.className");
     });
 
-    var output = nunjucks.renderString('{% extends "govuk/template.njk" %}', {});
-    expect(output).toContain("govuk-footer__meta-item");
-  });
+    test("test gov uk page rendering", () => {
+        nunjucks.configure(["node_modules/govuk-frontend/", "src/framework/"], {
+            autoescape: true
+        });
+        let context = new Context(null);
+        let page = new GovUkPage();
+        var output = page.render(context);
+        expect(output).toContain("govuk-template");
+    });
+
+    test("test nunjucks loading", () => {
+        nunjucks.configure(["node_modules/govuk-frontend/"], {
+            autoescape: true
+        });
+
+        var output = nunjucks.renderString("{% extends \"govuk/template.njk\" %}", {});
+        expect(output).toContain("govuk-footer__meta-item");
+    });
 })();
